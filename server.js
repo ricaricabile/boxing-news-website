baab42c8ee93c55a736d8f275b09f17c9d8ba153
ricaricabile <gabrielmamon288@gmail.com>
2024-06-15 03:46:28 +0800

    modify some files

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+if(process.env.NODE_ENV != 'production') require('dotenv').config();
+
 const axios = require('axios');
 
 const app = express();
@@ -11,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-if(process.env.NODE_ENV != 'production') require('dotenv').config();
+
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')));
 }
